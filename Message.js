1fd090f1913651c8b0dafd040f1e102a7f996215
Mounir <elb_mounir@hotmail.com>
2023-01-24 16:31:05 +0200

    Add message generator limited to 3 choices per list.

--- Message.js
+++ Message.js
@@ -1,9 +1,8 @@
-//Data for the quotation message stored in 3 lists
-const part_one =[];
-const part_two =[];
-const part_three =[];
-
-// function generating random message
-const message_generator = (array1,array2,array3) => {
-
-}
+// function generating random message from 3 lists
+const message_generator = () => {
+    const part_one =["If you are going through hell","Believe in your self!","If you can dream it"];
+    const part_two =["look deep inside you","Keep going","Never look back", ];
+    const part_three =["And you can find your way out","and you will beat your demons", "you are almost there"];
+    return console.log(`${part_one[Math.floor(Math.random()*part_one.length)]} ,${part_two[Math.floor(Math.random()*part_two.length)]} ,${part_three[Math.floor(Math.random()*part_two.length)]}.`);
+};
+message_generator();
