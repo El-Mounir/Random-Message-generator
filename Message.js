915540638096e0e2f219f75d3de9be5aec1cdbad
Mounir <elb_mounir@hotmail.com>
2023-01-24 15:55:47 +0200

    First commit

---
+++ Message.js
@@ -0,0 +1,9 @@
+//Data for the quotation message stored in 3 lists
+const part_one =[];
+const part_two =[];
+const part_three =[];
+
+// function generating random message
+const message_generator = (array1,array2,array3) => {
+
+}
